1506b9bbbce32e3ba520ef10b2e163a6419f2f02
Richard Snider <richardnsnider@gmail.com>
2021-10-29 19:43:05 -0700

    update golang install script

--- install/golang.sh
+++ install/golang.sh
@@ -4,4 +4,4 @@ set -ex
 # https://github.com/golang/go/wiki/Ubuntu
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
-sudo apt install golang-go
+sudo apt install --yes golang-go
