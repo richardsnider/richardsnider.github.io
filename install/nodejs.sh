e4faefe3225024c6745ccb108710986301664a61
Richard Snider <richardnsnider@gmail.com>
2021-10-29 20:04:09 -0700

    update node js install script

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -2,5 +2,5 @@
 set -ex
 
 # https://github.com/nodesource/distributions/blob/master/README.md#debinstall
-curl --silent https://deb.nodesource.com/setup_14.x | bash -
-sudo apt-get install -y nodejs
+curl --silent https://deb.nodesource.com/setup_16.x | sudo -E bash
+sudo apt-get install --yes nodejs
