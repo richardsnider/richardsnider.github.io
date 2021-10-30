5800e52eaf1061f52891f2de110020d8a37b6893
Richard Snider <richardnsnider@gmail.com>
2021-10-30 11:54:37 -0700

    default nodejs install to lts

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -2,5 +2,5 @@
 set -ex
 
 # https://github.com/nodesource/distributions/blob/master/README.md#debinstall
-curl --silent https://deb.nodesource.com/setup_16.x | sudo -E bash
-sudo apt-get install --yes nodejs
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
