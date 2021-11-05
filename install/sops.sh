5c8d857c9723afe8a9415442adc7da33791f6c4f
Richard Snider <richardnsnider@gmail.com>
2021-11-04 20:59:21 -0700

    sudo chmod command for sops

--- install/sops.sh
+++ install/sops.sh
@@ -4,4 +4,4 @@ set -ex
 export RELEASE_URL="https://api.github.com/repos/mozilla/sops/releases/latest"
 export DOWNLOAD_URL=$(curl --silent $RELEASE_URL | jq -r '.assets[] | select(.browser_download_url | contains(".linux")).browser_download_url')
 sudo curl -L $DOWNLOAD_URL --output /usr/bin/sops
-chmod +x /usr/bin/sops
+sudo chmod +x /usr/bin/sops
