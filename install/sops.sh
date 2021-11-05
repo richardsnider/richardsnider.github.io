cd93b84b41f6f41dae3fb256d01e220c9d61f63d
Richard Snider <richardnsnider@gmail.com>
2021-11-04 20:44:38 -0700

    fix sops install script

--- install/sops.sh
+++ install/sops.sh
@@ -2,4 +2,4 @@
 set -ex
 
 export RELEASE_URL="https://api.github.com/repos/mozilla/sops/releases/latest"
-sudo curl -L $(curl --silent $RELEASE_URL | jq -r '.assets[] | select(.browser_download_url | contains(".linux")).browser_download_url') > ~/usr/bin/sops
+sudo curl -L $(curl --silent $RELEASE_URL | jq -r '.assets[] | select(.browser_download_url | contains(".linux")).browser_download_url') > /usr/bin/sops
