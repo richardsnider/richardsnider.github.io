f4bf1230c865479f31cbc2fac0265bc7b458f0b3
Richard Snider <richardnsnider@gmail.com>
2022-05-10 11:46:13 -0700

    add vs code user settings

---
+++ vs-code-user-setttings.js
@@ -0,0 +1,45 @@
+(
+
+//######################
+//------ START ---------
+//######################
+
+{
+  "breadcrumbs.enabled": false,
+  "diffEditor.ignoreTrimWhitespace": false,
+  "editor.detectIndentation": false,
+  "editor.fontFamily": "'FreeMono', 'monospace', monospace, 'Droid Sans Fallback'",
+  "editor.minimap.enabled": false,
+  "editor.renderControlCharacters": true,
+  "editor.renderWhitespace": "all",
+  "editor.tabSize": 2,
+  "extensions.ignoreRecommendations": true,
+  "gitlens.codeLens.enabled": false,
+  "go.formatTool": "goimports",
+  "go.toolsManagement.autoUpdate": true,
+  "go.useLanguageServer": true,
+  "gopls": {
+    "build.env": {
+      "GOARCH": "amd64",
+      "GOOS": "linux",
+      // "GOPATH": "~/go"
+    }
+  },
+  "redhat.telemetry.enabled": false, // related to yaml extension
+  "terminal.integrated.cursorBlinking": true,
+  "terminal.integrated.enablePersistentSessions": false,
+  "terminal.integrated.fontFamily": "FreeMono",
+  "terminal.integrated.scrollback": 10000,
+  "window.menuBarVisibility": "compact",
+  "window.newWindowDimensions": "maximized",
+  "window.zoomLevel": 1,
+  "workbench.colorTheme": "Nicer High Contrast",
+  "workbench.editor.showTabs": true,
+  "workbench.statusBar.visible": false,
+}
+
+//######################
+//------ END ---------
+//######################
+
+)
