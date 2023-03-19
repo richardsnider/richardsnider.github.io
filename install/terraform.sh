742d60d0f18aefe6a416ebb5767bb39ad82588b4
Richard Snider <richardnsnider@gmail.com>
2023-03-19 08:21:09 -0700

    terraform install script

---
+++ install/terraform.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli#install-terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt-get install terraform
