c00323c1b2f5ae31fea113900977d6c6ed01fb17
richardsnider <richardsnider@protonmail.com>
2023-07-06 19:00:44 -0700

    update vscode install

--- install/vscode.sh
+++ install/vscode.sh
@@ -2,11 +2,11 @@
 set -ex
 
 # https://code.visualstudio.com/docs/setup/linux
+sudo apt-get install wget gpg apt-transport-https --yes
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-rm packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
 sudo apt update
-sudo apt install code
+sudo apt install code --yes
