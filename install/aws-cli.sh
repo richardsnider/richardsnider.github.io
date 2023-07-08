48136221c23518fdd7e514e8d2851107234a26dd
richardsnider <richardsnider@protonmail.com>
2023-07-08 08:20:38 -0700

    alter aws-cli install script

--- install/aws-cli.sh
+++ install/aws-cli.sh
@@ -4,7 +4,7 @@ set -ex
 sudo apt install curl unzip
 
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
-unzip $HOME/awscliv2.zip
-sudo $HOME/aws/install
-rm -rf $HOME/aws $HOME/awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip /tmp/awscliv2.zip
+sudo /tmp/aws/install
+rm -rf /tmp/aws /tmp/awscliv2.zip
