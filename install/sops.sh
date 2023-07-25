a82a163fcdfc1cb1457c4f10e8e74e9713dd1260
richardsnider <richardsnider@protonmail.com>
2023-07-24 18:13:42 -0700

    remove sops

--- install/sops.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-export RELEASE_URL="https://api.github.com/repos/mozilla/sops/releases/latest"
-export DOWNLOAD_URL=$(curl --silent $RELEASE_URL | jq -r '.assets[] | select(.browser_download_url | contains(".linux")).browser_download_url')
-sudo curl -L $DOWNLOAD_URL --output /usr/bin/sops
-sudo chmod +x /usr/bin/sops
