8e80030ccc12a8ceb11266a50907411b0ed27a06
richardsnider <richardsnider@protonmail.com>
2023-11-18 09:47:16 -0800

    update nodejs install script

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-# https://github.com/nodesource/distributions/blob/master/README.md#debinstall
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# https://github.com/nodesource/distributions#debian-and-ubuntu-based-distributions
+
+sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
